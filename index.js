1a35ea6981aa6e7f6eee95a676ccf8e2ebe75400
Matthew Riehm <mcr385@nau.edu>
2025-09-17 23:08:46 -0700

    Changed font, button styling, other issues and tweaks

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
 const express = require('express');
 const app = express();
 const port = 3000;
+
+require('./data.js');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -11,14 +12,9 @@ app.get('/', (req, res) => {
     res.render('index', { posts: posts });
 });
 
+const postsRouter = require('./routes/posts');
+app.use('/posts', postsRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-
-const postsRouter = require('./routes/posts');
-
-global.posts = [];
-
-app.use('/posts', postsRouter);
-
--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 
@@ -15,16 +14,21 @@ router.post('/', (req, res) => {
     res.redirect('/');
 });
 
-
 // Show Post
 router.get('/:id', (req, res) => {
     const post = posts.find(p => p.id === parseInt(req.params.id));
+    if (!post) {
+        return res.status(404).render('error', { message: 'Post not found' });
+    }
     res.render('show', { post });
 });
 
 // Edit Post Form
 router.get('/:id/edit', (req, res) => {
     const post = posts.find(p => p.id === parseInt(req.params.id));
+    if (!post) {
+        return res.status(404).render('error', { message: 'Post not found' });
+    }
     res.render('edit', { post });
 });
 
@@ -32,6 +36,9 @@ router.get('/:id/edit', (req, res) => {
 router.post('/:id', (req, res) => {
     const { title, content, author } = req.body;
     const post = posts.find(p => p.id === parseInt(req.params.id));
+    if (!post) {
+        return res.status(404).render('error', { message: 'Post not found' });
+    }
     post.title = title;
     post.content = content;
     post.author = author;
@@ -40,7 +47,11 @@ router.post('/:id', (req, res) => {
 
 // Delete Post
 router.post('/:id/delete', (req, res) => {
-    posts = posts.filter(p => p.id !== parseInt(req.params.id));
+    const postIndex = posts.findIndex(p => p.id === parseInt(req.params.id));
+    if (postIndex === -1) {
+        return res.status(404).render('error', { message: 'Post not found' });
+    }
+    posts.splice(postIndex, 1);
     res.redirect('/');
 });
 
